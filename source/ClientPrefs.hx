2ad311b48b51bca861f535ac0c8b21e178bcb120
MarioMaster2021 <95196453+MarioMaster2021@users.noreply.github.com>
2022-01-07 22:27:05 -0500

    1 fix

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -27,7 +27,7 @@ class ClientPrefs {
 	public static var imagesPersist:Bool = false;
 	public static var ghostTapping:Bool = true;
 	public static var hideTime:Bool = false;
-	public static var ekInput:Bool = false
+	public static var ekInput:Bool = false;
 
 	//Every key has two binds, these binds are defined on defaultKeys! If you want your control to be changeable, you have to add it on ControlsSubState (inside OptionsState.hx)'s list
 	public static var keyBinds:Map<String, Dynamic> = new Map<String, Dynamic>();
